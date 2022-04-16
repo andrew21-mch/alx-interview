8d5912a3c183a86f5a836bcf67637260344e611f
andrew21-mch <nfonandrew73@gmail.com>
2022-04-16 03:50:55 +0100

    box interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+def canUnlockAll(boxes):
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for box in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for index in range(len(boxes)):
+            boxes_checked = box in boxes[index] and box != index
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
+
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
